3b1eabf3f66a3640ca762dd6b82b1f9fff5b278d
JonnyPC <jon9028@gmail.com>
2020-12-08 13:54:28 +0200

    Converted main to fragment, added fragment for options, added base for navigation

---
+++ app/src/main/java/com/example/livemap/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.livemap
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MapActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+
+}
---
+++ app/src/main/java/com/example/livemap/social/OptionsViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.livemap.social
+
+import androidx.lifecycle.ViewModel
+
+class ActionViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+
+}
